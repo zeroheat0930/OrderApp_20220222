31b523c404d1d6f08f940416e49e2b6aad980a49
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 10:18:57 +0900

    피자가게 리스트뷰를 프래그먼트에 배치/ 한 줄의 모양을 xml파일만 추가

--- app/src/main/java/com/zeroheat/orderapp_20220222/MainActivity.kt
+++ app/src/main/java/com/zeroheat/orderapp_20220222/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
 
         mAdapter = MainViewPagerAdapter(supportFragmentManager)
         mainViewPager.adapter = mAdapter
+//      도전 과제 - 치킨 가게 목록 추가 시에는 , offscreenLimit = 3 설정 해줘야 정상 동작.
+
 
 
         mainTabLayout.setupWithViewPager(mainViewPager)
