931355fad77eabde50649cc4a874b7b5a06e750c
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 09:59:08 +0900

    get count, item 오버라이딩 구현실현

--- app/src/main/java/com/zeroheat/orderapp_20220222/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/zeroheat/orderapp_20220222/adapters/MainViewPagerAdapter.kt
@@ -3,18 +3,20 @@ package com.zeroheat.orderapp_20220222.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.zeroheat.orderapp_20220222.fragments.MyProfileFragment
+import com.zeroheat.orderapp_20220222.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
+    override fun getCount() = 2
+        //뷰페이저 몇장 나갈껀지
 
-
-
-    }
 
     override fun getItem(position: Int): Fragment {
 
-
-
+        return when(position) {
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
     }
 
 
