aabac6b072ab81773bcddb00b8d7cd74456d79e4
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 11:01:43 +0900

    getView 함수 기본 구조

--- app/src/main/java/com/zeroheat/orderapp_20220222/adapters/StoreAdapter.kt
+++ app/src/main/java/com/zeroheat/orderapp_20220222/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.zeroheat.orderapp_20220222.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.zeroheat.orderapp_20220222.R
 import com.zeroheat.orderapp_20220222.datas.StoreData
 
 class StoreAdapter(
@@ -9,4 +13,17 @@ class StoreAdapter(
     resId: Int,
     val mList: ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
 }
