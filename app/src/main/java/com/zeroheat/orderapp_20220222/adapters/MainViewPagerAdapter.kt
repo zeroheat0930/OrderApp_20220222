9c401bf938245b5c66dcd8ca5850251ceeac48d3
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 09:54:56 +0900

    뷰페이저 어댑터 클래스 제작중 - 상속 부분 해결

---
+++ app/src/main/java/com/zeroheat/orderapp_20220222/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.zeroheat.orderapp_20220222.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+
+
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+
+
+    }
+
+
+}
