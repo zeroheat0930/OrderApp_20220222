21a1769810c4e9401450f6079c8f259d9cb6e5db
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 10:51:11 +0900

    가게 데이터 표현용 클래스 추가

---
+++ app/src/main/java/com/zeroheat/orderapp_20220222/datas/StoreData.kt
@@ -0,0 +1,10 @@
+package com.zeroheat.orderapp_20220222.datas
+
+class StoreData(
+    val name : String,
+    val rating : Double,
+    val phoneNum : String,
+    val logoImagaURL : String,
+
+    ) {
+}
