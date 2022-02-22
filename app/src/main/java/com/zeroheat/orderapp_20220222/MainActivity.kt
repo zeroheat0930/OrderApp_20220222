ec90b29924014c41470583873c96174539548a8a
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 10:04:34 +0900

    뷰페이저 어댑터/ 뷰페이저 연결 작업

--- app/src/main/java/com/zeroheat/orderapp_20220222/MainActivity.kt
+++ app/src/main/java/com/zeroheat/orderapp_20220222/MainActivity.kt
@@ -2,10 +2,30 @@ package com.zeroheat.orderapp_20220222
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zeroheat.orderapp_20220222.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //lateinit은 무조껀 : 써야됨
+    lateinit var mAdapter: MainViewPagerAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mAdapter
+
     }
 }
