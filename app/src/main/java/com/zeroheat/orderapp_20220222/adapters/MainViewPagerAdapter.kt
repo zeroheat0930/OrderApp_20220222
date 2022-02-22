297827068e58e8b246de948fa9b2342c132fd0eb
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 10:11:56 +0900

    페이지별 제목 설정

--- app/src/main/java/com/zeroheat/orderapp_20220222/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/zeroheat/orderapp_20220222/adapters/MainViewPagerAdapter.kt
@@ -20,4 +20,13 @@ class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
     }
 
 
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        return when(position){
+            0 -> "피자 주문"
+            else -> "내 정보 설정"
+        }
+    }
+
+
 }
