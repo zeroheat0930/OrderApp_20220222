f0abe3325d88305a53720d20b074f536d2214ca8
zeroheat0930 <dongjunkorea@naver.com>
2022-02-22 15:20:54 +0900

    전화 주문 버튼 클릭> 권한 물어보기만

--- app/src/main/java/com/zeroheat/orderapp_20220222/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/zeroheat/orderapp_20220222/ViewStoreDetailActivity.kt
@@ -1,8 +1,15 @@
 package com.zeroheat.orderapp_20220222
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.zeroheat.orderapp_20220222.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
+
 
 class ViewStoreDetailActivity : AppCompatActivity() {
 
@@ -19,8 +26,37 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
     fun setupEvents(){
 
+        val pl = object : PermissionListener{
+            override fun onPermissionGranted() {
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@ViewStoreDetailActivity,"통화 권한이 없습니다.",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        btnCall.setOnClickListener {
+
+        TedPermission.create()
+            .setPermissionListener(pl)
+            .setPermissions(Manifest.permission.CALL_PHONE)
+            .check()
+        }
+
     }
     fun setValues(){
+
+        Glide.with(this).load(mStoreData.logoImageURL).into(imgLogo)
+
+        txtStoreName.text = mStoreData.name
+
+        txtPhoneNum.text = mStoreData.phoneNum
+
+        ratingBar.rating = mStoreData.rating.toFloat()
+
+        txtRating.text = "(${mStoreData.rating})"
 
     }
 
